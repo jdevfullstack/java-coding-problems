7e0e78417242e2d414b1764d26108d9919bfd730
jdevstatic <jtechdev001@gmail.com>
2022-08-04 14:53:05 +0800

    Update StringContainsVowels.java

--- src/StringContainsVowels.java
+++ src/StringContainsVowels.java
@@ -1,1 +1,23 @@
+public class StringContainsVowels {
 
+	public static boolean 
+		stringContainsVowels(String input) {
+
+		return input.toLowerCase().matches(".*[aeiou].*");
+
+	}	
+
+	public static void main(String[] args) {
+
+		String input = "fdfd";
+
+		System.out.print(input + " ");
+
+		if (stringContainsVowels(input) == true) {
+		System.out.println("contains vowel"); 
+		} else {
+		System.out.println("does not contain vowel");
+		}
+	}
+
+}
