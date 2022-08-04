66822bd62edb1761782f09e764bc75106081f74e
jdevstatic <jtechdev001@gmail.com>
2022-08-04 18:23:17 +0800

    add BinarySearch.java

---
+++ src/BinarySearch.java
@@ -0,0 +1,42 @@
+public class BinarySearch {
+
+        public static int binarySearch(int arr[], int low, int high, int key) {
+        int mid = (low + high) / 2;
+
+        while (low <= high) {
+            if (arr[mid] < key) {
+                low = mid + 1;
+            } else if (arr[mid] == key) {
+                return mid;
+            } else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+        }
+        if (low > high) {
+            return -1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int intArray[] = {7, 8, 9};
+
+        int key = 5;
+
+        if (binarySearch(intArray, 0, 5, key) != -1) {
+
+            System.out.print("searching `");
+            System.out.print(key);
+            System.out.print("` in the array... it's at index ");
+            System.out.println(binarySearch(intArray, 0, 5, key));
+            
+
+        } else {
+
+            System.out.println("index not found");
+
+        }
+    }
+
+}
