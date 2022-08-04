2e8d915adda55287d26694b9e20d6e7c8bf082c9
jdevstatic <jtechdev001@gmail.com>
2022-08-04 08:39:05 +0000

    add SortArray.java

---
+++ src/SortArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, -1, -2, 4};
+
+        Arrays.sort(array);
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
