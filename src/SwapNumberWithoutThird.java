9d53f18d13235c92755e46f09bf501b5581b3319
jdevstatic <jtechdev001@gmail.com>
2022-08-04 14:52:08 +0800

    Update SwapNumberWithoutThird.java

--- src/SwapNumberWithoutThird.java
+++ src/SwapNumberWithoutThird.java
@@ -1,1 +1,16 @@
+public class SwapNumberWithoutThird {
 
+    public static void swapNumbers(int a, int b) {
+        b = b + a;
+        a = b - a;
+        b = b - a;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swapNumbers(a, b);
+        System.out.printf("a is %d, b is %d %n", a, b); // a is 10, b is 20
+    }
+
+}
